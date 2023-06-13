fa8aa20c903cd865af8aa4d4305df0a29e2a15d1
helmigsimon <40271543+helmigsimon@users.noreply.github.com>
2023-06-13 07:38:07 +0100

    Feat: Adding some additional analysis functions and extending .gitignore

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from src import paths
 
 
@@ -30,6 +31,25 @@ def get_average_word_count_per_comment_per_user(df: pd.DataFrame) -> pd.DataFram
 def get_average_comments_per_pull_request_per_user(df: pd.DataFrame) -> pd.DataFrame:
     """Get average comments per pull request per user"""
     return df.groupby(['user', 'pull_request_number']).size().groupby('user').mean().sort_values(ascending=False)
+
+def plot_comment_count_per_day_by_user(df: pd.DataFrame):
+    """Plot comment count per day by user"""
+    grouped = df.groupby(['user', 'created_at']).size().reset_index(name='counts')
+    for user in grouped['user'].unique():
+        user_data = grouped[grouped['user'] == user]
+        plt.plot(user_data['created_at'], user_data['counts'], label=user)
+
+    plt.xlabel('Date')
+    plt.ylabel('Number of comments')
+    plt.title('Number of comments per day per user')
+    plt.legend()
+    plt.show()
+
+def get_top_commenting_users(df: pd.DataFrame) -> list[str]:
+    """Get top commenting users"""
+    comment_counts = get_comment_percentages_per_user_(df)
+
+    
 
 if __name__ == '__main__':
     df = import_data()
